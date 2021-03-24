1997e9d6e08861e066af8eb6bfee6562f2453e47
fulecorafa <ra.pha@live.com>
2021-03-24 07:20:01 -0300

    Single token tokenizer, autobuild with packages

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 PROJECT_NAME=Jlox
+PROJECT_PATH=src/
+FILES=$(shell find ${PROJECT_PATH} -name "*.java")
 
 run: build
-	java -cp ./dist/class ${PROJECT_NAME} ${ARGS}
+	java -cp ./dist/class ${PROJECT_NAME}/${PROJECT_NAME} ${ARGS}
 
 build:
-	javac src/${PROJECT_NAME}.java -d dist/class
+	javac -d dist/class ${FILES}
 	echo -e '\033[0;32mBuilding finished\033[0m'
---
+++ src/Jlox/Jlox.java
@@ -0,0 +1,64 @@
+package Jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import Jlox.Compiler.*;
+
+public class Jlox {
+  static boolean hadError = false;
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  public static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+    if (hadError) System.exit(65);
+  }
+
+  public static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    while (true) {
+      System.out.println("Jlox: $ ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+
+    System.out.println("Jlox: Bye bye!");
+  }
+
+  public static void run(String code) throws IOException {
+    Scanner scanner = new Scanner(code);
+    List<Token> tokens = scanner.scanTokens();
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  public static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  public static void report(int line, String where, String message) {
+    System.err.println(
+      "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+}
